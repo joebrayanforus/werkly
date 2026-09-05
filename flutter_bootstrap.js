275c0d9766bc039ace5c83994128c9170b1a06e3
joebrayanforus <joebrayanforus@users.noreply.github.com>
2026-09-05 21:07:31 +0200

    Redeploy web app with Open Graph link-preview tags

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2342000084" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4229568812" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
